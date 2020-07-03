5f6af5624010a83d66da74afaf5d9b9a441d91c9
MasashiKiyomi <51880535+MasashiKiyomi@users.noreply.github.com>
2020-07-03 09:20:54 +0900

    Create thread.py
    A simple sample of threading.
    I want to display each 3x3 letters as a block.
    Please help me!

---
+++ thread.py
@@ -0,0 +1,30 @@
+from threading import Thread
+
+def my_function1():
+  for i in range(10)
+    print("123")
+    print("456")
+    print("789")
+
+def my_function2():
+  for i in range(10)
+    print("abc")
+    print("def")
+    print("ghi")
+
+def my_function3():
+  for i in range(10)
+    print("ABC")
+    print("DEF")
+    print("GHI")
+
+if __name__ == "__main__":
+  t1 = Thread(target = my_function1)
+  t2 = Thread(target = my_function2)
+  t3 = Thread(target = my_function3)
+  t1.start()
+  t2.start()
+  t3.start()
+  t1.join()
+  t2.join()
+  t3.join()
