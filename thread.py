758f5d0653595ab0fa689d692b177fccdc24c419
yubawarabi <67773546+yubawarabi@users.noreply.github.com>
2020-07-03 13:57:46 +0900

    Update thread.py
    Add RLock

--- thread.py
+++ thread.py
@@ -1,22 +1,25 @@
-from threading import Thread
+from threading import RLock,Thread
 
 def my_function1():
   for i in range(1000):
-    print("123")
-    print("456")
-    print("789")
+    with lock:
+      print("123")
+      print("456")
+      print("789")
 
 def my_function2():
   for i in range(1000):
-    print("abc")
-    print("def")
-    print("ghi")
+    with lock:
+      print("abc")
+      print("def")
+      print("ghi")
 
 def my_function3():
   for i in range(1000):
-    print("ABC")
-    print("DEF")
-    print("GHI")
+    with lock:
+      print("ABC")
+      print("DEF")
+      print("GHI")
 
 if __name__ == "__main__":
   t1 = Thread(target = my_function1)
